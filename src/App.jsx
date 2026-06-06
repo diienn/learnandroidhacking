c228dcc973d4df96d16026919ae9caa485944208
diienn <alfredoballs67@atomicmail.io>
2026-06-05 22:10:30 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+import { createClient } from "@supabase/supabase-js";
+
+
 const SUPABASE_URL  = import.meta.env.VITE_SUPABASE_URL;
 const SUPABASE_ANON = import.meta.env.VITE_SUPABASE_ANON_KEY;
 const sb = createClient(SUPABASE_URL, SUPABASE_ANON);
